704ea9c47456efb157496d5d095e1c5cef2e101d
Sebastian Velez <sebasdj13@outlook.com>
2026-04-19 13:14:43 -0500

    feat: add r2 next config to images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-ea986fd84e9d4e7aa3484307ea5f02b1.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
